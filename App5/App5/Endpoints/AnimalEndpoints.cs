652ffc3bde2b1b15b55ca2b78c7fddf4b3f66eee
Dominik Chechowski <che.dom29@gmail.com>
2024-04-09 11:02:39 +0200

    added minimal api

---
+++ App5/App5/Endpoints/AnimalEndpoints.cs
@@ -0,0 +1,18 @@
+﻿namespace App5.Endpoints;
+
+public static class AnimalEndpoints
+{
+    public static void MapAnimalEndpoints(this WebApplication app)
+    {
+        //minimal api
+        app.MapGet("/animals/{id}", (int id) =>
+        {
+            return Results.Ok(id);
+
+        });
+        app.MapPost("/animals", () =>
+        {
+            return Results.Created();
+        });
+    }
+}
